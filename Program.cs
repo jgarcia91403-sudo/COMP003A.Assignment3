28cdf6556f47bb6589c84efcddc599b1a13dc906
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-01-30 14:56:05 -0800

    I have completed makeing the yes and no questions have to go back and work on calculating the age and the rediness category.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace COMP003A.Assignment3
+﻿using System.ComponentModel.Design;
+
+namespace COMP003A.Assignment3
 {
     internal class Program
     {
@@ -10,6 +12,26 @@
             Console.ReadLine();
             Console.Write("Enter your birth year:");
             Console.ReadLine();
+            int birthyear = DateTime.Now.Year;
+            int age = currentyear - birthyear;
+            Console.WriteLine("\ncalculate your age:");
+            string readinessCategory;
+            if (1 < 18) ;
+            {
+                readinessCategory = "Minor";
+            }
+            if (19 <= 64);
+            {
+                readinessCategory = "Adult";
+            }
+           
+            {
+                readinessCategory = "senior";
+            }
+            Console.Write("Do you have a valid ID? (yes/no): ");
+            bool hasValidID = Console.ReadLine().ToLower() == "yes";
+            Console.Write("Have you completed orientation? (yes/no):");
+            bool completedOrientation = Console.ReadLine().ToLower() == "yes";
         }
     }
 }
