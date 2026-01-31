c1fb5baa3d0e0b5a3567ee5c96c696a0f1b14196
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-01-30 18:15:28 -0800

    Finished putting the choice to were you get an answer

--- Program.cs
+++ Program.cs
@@ -12,33 +12,36 @@ namespace COMP003A.Assignment3
             Console.ReadLine();
             Console.Write("Enter your birth year:");
             Console.ReadLine();
-            int birthyear = DateTime.Now.Year;
-            int age = currentyear - birthyear;
-            Console.WriteLine("\ncalculate your age:");
+            Console.WriteLine($"calculate your age:");
             string readinessCategory;
-            if (age > 18) ;
-            {
-                readinessCategory = "Minor";
-            }
-            if (19 <= 64);
-            {
-                readinessCategory = "Adult";
-            }
-           
-            {
-                readinessCategory = "senior";
-            }
             Console.Write("Do you have a valid ID? (yes/no): ");
             bool hasValidID = Console.ReadLine().ToLower() == "yes";
             Console.Write("Have you completed orientation? (yes/no):");
             bool completedOrientation = Console.ReadLine().ToLower() == "yes";
+
             Console.WriteLine("select a guidance option:");
             Console.WriteLine("1 - Academic planning");
             Console.WriteLine("2 - Personal planning");
             Console.WriteLine("3 - Career planning");
             Console.Write("Enter choice:");
-            Console.ReadLine();
-            string guidanceOption = Console.ReadLine();
+            string choice = Console.ReadLine();
+            Console.WriteLine("\n Guidance:");
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Meet with an academic advisor to plan your next term");
+                    break;
+                case "2":
+                    Console.WriteLine("Focus on time management and personal goal setting");
+                    break;
+                case "3":
+                    Console.WriteLine("Explore internships and build a professional resume");
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. please restart and choose a valid option");
+                    break;
+            }
             
         }
     }
